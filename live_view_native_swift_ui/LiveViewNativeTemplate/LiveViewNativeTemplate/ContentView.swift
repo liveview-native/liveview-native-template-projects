776496c7face1cbec12e67fc3b96b980cdefd195
Carson Katri <Carson.katri@gmail.com>
2023-09-07 17:22:31 -0400

    Update ContentView.swift
    Change structure of production URL loading

--- live_view_native_swift_ui/LiveViewNativeTemplate/LiveViewNativeTemplate/ContentView.swift
+++ live_view_native_swift_ui/LiveViewNativeTemplate/LiveViewNativeTemplate/ContentView.swift
@@ -2,9 +2,12 @@ import SwiftUI
 import LiveViewNative
 
 struct ContentView: View {
+    static let productionURL = (Bundle.main.object(forInfoDictionaryKey: "Phoenix Production URL") as? String)
+        .flatMap(URL.init) ?? URL(string: "example.com")!
+    
     var body: some View {
         LiveView(
-            .automatic(URL(string: Bundle.main.object(forInfoDictionaryKey: "Phoenix Production URL") as? String ?? "example.com")!)
+            .automatic(development: .localhost, production: .custom(Self.productionURL))
         )
     }
 }
